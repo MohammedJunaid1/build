4c74c9f54350267d780070aa2983258b71e71642
nabeel-ahsan <itsme007nabeel@gmail.com>
2024-08-09 22:29:13 +0530

    Created Navbar component

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,27 @@
+import React from "react";
 
 export default function Navbar() {
-  return (
-    <div className="bg-green-500">
-      Navbar
-    </div>
-  )
+    return (
+        <nav className="bg-neutral-800 p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="text-white text-xl font-bold">
+                    ChatGPT
+                </div>
+                <div className="flex space-x-4">
+                    <a href="#" className="text-gray-300 hover:text-white">
+                        Home
+                    </a>
+                    <a href="#" className="text-gray-300 hover:text-white">
+                        Features
+                    </a>
+                    <a href="#" className="text-gray-300 hover:text-white">
+                        Pricing
+                    </a>
+                    <a href="#" className="text-gray-300 hover:text-white">
+                        Contact
+                    </a>
+                </div>
+            </div>
+        </nav>
+    );
 }
